5d1101b25cbb52e30b3a6934e98a5a288b11d000
= <=>
2017-04-05 12:03:54 +0200

    Avancement

--- TD02/crabs.R
+++ TD02/crabs.R
@@ -7,11 +7,50 @@ crabBF = crabsquant[50:100,]
 crabOM = crabsquant[100:150,]
 crabOF = crabsquant[150:200,]
 
-
-#####Male and Female
 crabM = rbind(crabBM, crabOM)
 crabF = rbind(crabBF, crabOF)
 
+crabO = rbind(crabOF, crabOM)
+crabB = rbind(crabBF, crabBM)
+png(filename = "caracteristiquesMF.png")
+par(mar=c(5, 4, 4, 6), xpd=TRUE)
+boxplot(list(crabM$FL, crabF$FL,crabM$RW, crabF$RW,crabM$CL, crabF$CL,crabM$CW, crabF$CW,crabM$BD, crabF$BD), 
+        col = c('forestgreen','forestgreen','darkmagenta','darkmagenta','gold','gold','deepskyblue4','deepskyblue4','darkred','darkred'),
+        method="jitter",
+        vertical=T,
+        ylab="size(mm)",
+        las=2,
+        main="Caractéristiques des crabes - Différence Male/Female",
+        names = c('male','female','male','female','male','female','male','female','male','female'))
+legend(11,50,xpd=NA,inset=.05, title="Legend",
+       c("FL","RW","CL","CW","BD"), 
+       fill=c("forestgreen","darkmagenta","gold","deepskyblue4","darkred"))
+dev.off()
+png(filename = "caracteristiquesOB.png")
+par(mar=c(5, 4, 4, 6), xpd=TRUE)
+boxplot(list(crabO$FL, crabB$FL,crabO$RW, crabB$RW,crabO$CL, crabB$CL,crabO$CW, crabB$CW,crabO$BD, crabB$BD), 
+        col = c('forestgreen','forestgreen','darkmagenta','darkmagenta','gold','gold','deepskyblue4','deepskyblue4','darkred','darkred'),
+        method="jitter",
+        vertical=T,
+        ylab="size(mm)",
+        main="Caractéristiques des crabes 
+        Différence des espèces Orange/Blue",
+        las=2,
+        names = c('Orange','Blue','Orange','Blue','Orange','Blue','Blue','Blue','Orange','Blue'))
+legend(11,50,xpd=NA,inset=.05, title="Legend",
+       c("FL","RW","CL","CW","BD"), 
+       fill=c("forestgreen","darkmagenta","gold","deepskyblue4","darkred"))
+dev.off()
+png(filename="sexe-crabs.png")
+par
+plot(crabsquant, main="Graphique bidimensionnel des caractéristiques 
+     des crabes male/female", bg=c("gold","darkred")[crabs[,2]], pch=c(21,22)[crabs[,2]])
+dev.off()
+png(filename="spicies-crabs.png")
+plot(crabsquant,main="Graphique bidimensionnel des caractéristiques 
+     des crabes orange/blue", bg=c("blue","darkorange")[crabs[,1]], pch=c(21,22)[crabs[,1]])
+dev.off()
+#####Male and Female
 #attach(crabM)
 #inter<-seq(min(CL),max(CL),by=(max(CL)-min(CL))/5)
 #hist(CL,breaks=inter, main = "Male longueur carapace")
@@ -39,9 +78,6 @@ boxplot(cbind(crabM$BD,crabF$BD ), main = "Profondeur du corps", names = c("Male
 #boxplot(cbind(crabM$RW,crabF$RW ), main = "Taille du train arrière")
 
 #####Bleu and Orange#######
-
-crabO = rbind(crabOF, crabOM)
-crabB = rbind(crabBF, crabBM)
 
 #attach(crabO)
 #inter<-seq(min(BD),max(BD), by=(max(BD)-min(BD))/5)
